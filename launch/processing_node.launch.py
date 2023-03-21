1d8bc5b5becaf5314281aad20100e5aa4e73480d
Tjark Schuette <tschuette@atb-potsdam.de>
2023-03-21 12:36:37 +0000

    changed camera/cloud frame

--- launch/processing_node.launch.py
+++ launch/processing_node.launch.py
@@ -11,14 +11,14 @@ def generate_launch_description():
             parameters=[
                 {"cloud_topic": "/cloud"},
                 {"world_frame": "map"},
-                {"camera_frame": "cloud_adjusted"},
-                {"voxel_leaf_size": 0.02}, # mm
-                {"x_filter_min": 0.1},    # mm
-                {"x_filter_max": 20.0},     # mm
-                {"y_filter_min": -3.0},    # mm
-                {"y_filter_max": 3.0},     # mm
-                {"z_filter_min": -2.5},    # mm
-                {"z_filter_max": 4.0},     # mm
+                {"camera_frame": "cloud"},
+                {"voxel_leaf_size": 0.02}, # m
+                {"x_filter_min": 0.1},    # m
+                {"x_filter_max": 20.0},     # m
+                {"y_filter_min": -3.0},    # m
+                {"y_filter_max": 3.0},     # m
+                {"z_filter_min": -2.5},    # m
+                {"z_filter_max": 4.0},     # m
                 {"plane_max_iterations": 100},
                 {"plane_distance_threshold": 0.03},
                 {"cluster_tolerance": 0.02},
